bd74ce6f2c1adee894e1ad4693b4c02d8f34a45e
Dale Cabarle <Danntabachoyy@gmail.com>
2026-03-04 19:50:38 +0800

    init react router & sidebar

---
+++ src/routes/home/Home.tsx
@@ -0,0 +1,21 @@
+import PokemonCards from "../../components/PokemonCards";
+import { ThemeSwitch } from "../../components/ThemeSwitch";
+import { usePokemon } from "../../hooks/usePokemon";
+
+const Home = () => {
+  const { loading, pokemons } = usePokemon(20);
+  if (loading) {
+    return <h1>fetching data...</h1>;
+  }
+
+  return (
+    <>
+      <div className="p-4 border mx-auto">
+        <ThemeSwitch />
+        <PokemonCards pokemons={pokemons} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
